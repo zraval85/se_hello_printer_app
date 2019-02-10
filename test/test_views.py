64a53a3720c399bb9aa3fa39b81012f27c7c838d
klonczyk <klonczyk@users.noreply.github.com>
2019-02-10 14:02:52 +0100

    Add xml output

--- test/test_views.py
+++ test/test_views.py
@@ -15,3 +15,8 @@ class FlaskrTestCase(unittest.TestCase):
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
         self.assertEquals('{ "imie":"Kornelia", "msg":"Witaj swiecie!"}', rv.data)
+
+    def test_msg_with_output_xml(self):
+        rv = self.app.get('/?output=xml')
+        self.assertEquals('<greetings> <name>' "Kornelia" '</name> <msg>'
+                                "Witaj swiecie!" '</msg> </greetings>', rv.data)
